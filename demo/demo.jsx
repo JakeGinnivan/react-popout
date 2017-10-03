4584a441011d856fe4e8f65b032cee08ed7af2d5
marchaos <marchaos@gmail.com>
2017-10-03 09:58:12 +0100

    Fixed issue with loading popouts when about:blank is used (no url). Demos all working now.

--- demo/demo.jsx
+++ demo/demo.jsx
@@ -2,37 +2,37 @@ import React from 'react';
 import Popout from '../lib/react-popout.jsx';
 
 export default class Example extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.popout = this.popout.bind(this);
     this.incrementTimer = this.incrementTimer.bind(this);
     this.popoutClosed = this.popoutClosed.bind(this);
     this.popoutContentClicked = this.popoutContentClicked.bind(this);
-    this.state = {isPoppedOut: false, timer: 0};
+    this.state = { isPoppedOut: false, timer: 0 };
   }
 
-  incrementTimer(){
-    var newTimer = this.state.timer + 1;
-    this.setState({timer: newTimer});
+  incrementTimer() {
+    let newTimer = this.state.timer + 1;
+    this.setState({ timer: newTimer });
   }
 
-  popout(){
-    this.setState({isPoppedOut: true, timerId: setInterval(this.incrementTimer, 1000)});
+  popout() {
+    this.setState({ isPoppedOut: true, timerId: setInterval(this.incrementTimer, 1000) });
   }
 
-  popoutClosed(){
-    if (this.state.timerId){
+  popoutClosed() {
+    if (this.state.timerId) {
       clearInterval(this.state.timerId);
-      this.setState({isPoppedOut: false, timerId: null, timer: 0});
+      this.setState({ isPoppedOut: false, timerId: null, timer: 0 });
     }
   }
 
-  popoutContentClicked(){
+  popoutContentClicked() {
     this.popoutClosed();
   }
 
-  render(){
-    if (this.state.isPoppedOut){
+  render() {
+    if (this.state.isPoppedOut) {
       return (
         // Remove url parameter to see about:blank support
         <Popout title='Test' onClosing={this.popoutClosed}>
@@ -45,7 +45,8 @@ export default class Example extends React.Component {
     } else {
       return (
         <div>
-          <strong>Example: Only render popout <a style={{ textDecoration: 'underline', color: 'blue', cursor: 'pointer' }} onClick={this.popout}>(pop window out)</a></strong>
+          <strong>Example: Only render popout <a style={{ textDecoration: 'underline', color: 'blue', cursor: 'pointer' }}
+                                                 onClick={this.popout}>(pop window out)</a></strong>
           <div>Demo 1 - Inline content</div>
         </div>
       );
